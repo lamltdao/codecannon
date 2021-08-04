d666b48188fc4cf182446bafcfa903f29fc152f8
lam-mac-20 <daoletunglam2002@gmail.com>
2021-08-04 14:56:25 +0700

    use env var for host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = {
-    host: "lam-app.xyz"
+    host: ENV["DOMAIN_NAME"]
   }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
